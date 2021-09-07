1c174cf217e482dfa901dcae634e524b7e0d5207
Angel Collaguazo <santijonaco@hotmail.com>
2021-09-06 20:08:30 -0500

    maven

---
+++ src/main/java1/ejercicios/variables/locales/globales/Paciente.java
@@ -0,0 +1,21 @@
+package ejercicios.variables.locales.globales;
+
+public class Paciente {
+	
+	private String nombre;
+	private int edad;
+	
+	
+	
+	public void chequeo() {
+		this.nombre="Javier";
+		String diagnostico = "completo";
+		diagnostico = "sobrepeso";
+		diagnostico = "diabetes";
+		
+		String medicina = "Realizar";
+		medicina = "Actividad Fisica";
+		medicina = "alimentos sin mucha azucar";
+	}
+
+}
