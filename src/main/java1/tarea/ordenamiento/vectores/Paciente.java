9033343016ff568a174b32c9eb7b39fe54155d97
Angel Collaguazo <santijonaco@hotmail.com>
2021-09-10 17:51:23 -0500

    tarea consultorio

--- src/main/java1/tarea/ordenamiento/vectores/Paciente.java
+++ src/main/java1/tarea/ordenamiento/vectores/Paciente.java
@@ -1,48 +1,53 @@
 package tarea.ordenamiento.vectores;
 
-public class Paciente implements Comparable<Paciente>{
+public class Paciente implements Comparable<Paciente> {
 
 	private String nombre;
 	private String apellido;
 	private int edad;
 	private String sintoma;
-	
-	
-		
-	
+
 	public Paciente(String nombre, String apellido, int edad, String sintoma) {
-		this.nombre=nombre;
-		this.apellido=apellido;
-		this.edad=edad;
-		this.sintoma=sintoma;
-		
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.sintoma = sintoma;
+
 		// TODO Auto-generated constructor stub
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
 	public String getSintoma() {
 		return sintoma;
 	}
+
 	public void setSintoma(String sintoma) {
 		this.sintoma = sintoma;
 	}
-	
+
 	public int compareTo(Paciente o) {
 		// TODO Auto-generated method stub
 		// 3 posibles retornos
@@ -58,12 +63,13 @@ public class Paciente implements Comparable<Paciente>{
 		}
 
 	}
+
 	@Override
 	public String toString() {
 		return "Paciente [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", sintoma=" + sintoma
 				+ "]";
 	}
-	
+
 }
 
 
