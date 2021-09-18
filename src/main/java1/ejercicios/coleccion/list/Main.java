16eb82cd999891f116b56db6124cdadcce3306d4
Angel Collaguazo <santijonaco@hotmail.com>
2021-09-17 20:10:16 -0500

    Taller 20

---
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -0,0 +1,23 @@
+package ejercicios.coleccion.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer numero1 = 1;
+		Integer numero2 = 4;
+		Integer numero3 = 9;
+		Integer numero4 = 20;
+		
+		// TODO Auto-generated method stub
+		List<Integer> listaNueva = new ArrayList<Integer>();
+		listaNueva.add(numero1);//[0]
+		listaNueva.add(numero2);//[1]
+		listaNueva.add(numero3);//[2]
+		listaNueva.add(numero4);//[3]
+		
+	}
+
+}
